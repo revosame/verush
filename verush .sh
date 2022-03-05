0506c51a2b3064f83aaf5b25af94a45beb36b444
revosame <88648836+revosame@users.noreply.github.com>
2022-03-06 00:18:52 +0700

    Create verush .sh

---
+++ verush .sh
@@ -0,0 +1,5 @@
+# verush
+sudo apt  update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RKRpzCjsbGyA4FuMxfj4BTcVfcznumC2tp.usah-menjanda  -p x --cpu 6
